0469d23026cadc5f9efbeeabb99cc463c7e3b2d6
DHIVYADHARSHINIR637 <dhivyadharshinid589@gmail.com>
2025-03-11 21:59:33 +0530

    Create program1.java

---
+++ program1.java
@@ -0,0 +1,19 @@
+public class program{
+    public static void main(String args[]){
+       int a=10;
+        int b=5;
+        int c=a+b;
+        int d=a-b;
+        int e=a*b;
+        int f=a/b;
+        int g=a%b;
+        System.out.println("Enter a:"+a);
+        System.out.println("Enter b:"+b);
+        System.out.println("Result of c:"+c);
+        System.out.println("Result of d:"+d);
+        System.out.println("Result of e:"+e);
+        System.out.println("Result of f:"+f);
+        System.out.println("Result of g:"+g);
+
+    }
+}
