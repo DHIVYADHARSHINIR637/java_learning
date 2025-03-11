1f37531add3923fa7399ecb511c7d72f3fb18a32
DHIVYADHARSHINIR637 <dhivyadharshinid589@gmail.com>
2025-03-11 22:33:10 +0530

    Create demo1.java

---
+++ demo1.java
@@ -0,0 +1,15 @@
+public class demo{
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        System.out.println("Enter a:"+a);
+        System.out.println("enter b:"+b);
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+
+    }
+}
