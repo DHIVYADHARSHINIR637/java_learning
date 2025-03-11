6ad0ecd783a4282c7d34a0d5a9fe84b2aaafa677
DHIVYADHARSHINIR637 <dhivyadharshinid589@gmail.com>
2025-03-11 22:18:32 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,10 @@
+public class demo{
+    public static void main(String args[]){
+        int a=10;
+        a++;
+        System.out.println(a);
+        a--;
+        System.out.println(a);
+
+    }
+}
