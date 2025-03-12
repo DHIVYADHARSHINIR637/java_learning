df445165f9a0d0ed6a8df8a4f0a93e100b44d1e0
DHIVYADHARSHINIR637 <dhivyadharshinid589@gmail.com>
2025-03-12 18:51:51 +0530

    Create program3.java

---
+++ program3.java
@@ -0,0 +1,13 @@
+public class program{
+    public static void main(String args[]){
+       int length=26;
+       int width=20;
+       int c=length*width;
+       System.out.println("Enter the length:");
+       System.out.println("Enter the width:");
+       System.out.println("Area of rectangle:"+c);
+
+    }
+    
+    
+}
