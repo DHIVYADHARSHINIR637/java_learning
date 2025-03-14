3f7ce07cd830c670d95efabe4bb5d7e03c0530b1
DHIVYADHARSHINIR637 <dhivyadharshinid589@gmail.com>
2025-03-14 21:18:37 +0530

    Create Demo6.java

---
+++ Demo6.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Demo6{
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the string");
+        String meghana=scan.nextLine();
+        if(meghana.equals("dead"))
+        {
+            System.out.println("surya meets ramya");
+
+        }
+        else
+        {
+            System.out.println("surya weds meghana");
+        }
+    }
+}
