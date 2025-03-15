4c6e7dbfa3ae36b19a142e026a335b5f16d9b396
DHIVYADHARSHINIR637 <dhivyadharshinid589@gmail.com>
2025-03-15 21:24:52 +0530

    Create Demo9.java

---
+++ Demo9.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Demo9{
+    public static void main(String args[])
+    {
+    Scanner scan=new Scanner(System.in);
+    int b=scan.nextInt();
+     for(int n=b;n>=1;n=n-1)
+        {
+            System.out.println("Number"+n);
+         
+        }
+    }
+
+
+
+}
