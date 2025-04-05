8716a1b42d843685683c95bad802fab694522169
DHIVYADHARSHINIR637 <dhivyadharshinid589@gmail.com>
2025-04-05 08:56:19 -0700

    Create demo19.java

---
+++ demo19.java
@@ -0,0 +1,20 @@
+package java_learning;
+
+public class addition {
+	int a=10;
+	int b=20;
+	void sum()
+	{
+		int c=a+b;
+		System.out.println("result of c:"+c);
+	}
+
+
+public static void main(String args[])
+{
+		addition a1=new addition();
+		a1.sum();
+}
+}
+
+
