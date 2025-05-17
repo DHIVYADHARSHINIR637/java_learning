79c88aa667a759a2a1301cc4c916cb016b25ecee
DHIVYADHARSHINIR637 <dhivyadharshinid589@gmail.com>
2025-05-17 18:31:44 +0530

    Create Prog33.java

---
+++ Prog33.java
@@ -0,0 +1,21 @@
+package dhivya;
+class Mobile{
+	void iphone()
+	{
+		System.out.println("this is iphone");
+	}
+	void samsung()
+	{
+		System.out.println("this ia samsung");
+	}
+	
+}
+public class Prog33 {
+	public static void main(String args[])
+	{
+		Mobile obj=new Mobile();
+		obj.iphone();
+		obj.samsung(); 
+	}
+
+}
