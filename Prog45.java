071ce50deaad00062f4fcc3ec41971f1b533d7e9
DHIVYADHARSHINIR637 <dhivyadharshinid589@gmail.com>
2025-05-18 11:23:55 +0530

    Create Prog45.java

---
+++ Prog45.java
@@ -0,0 +1,23 @@
+package dhivya;
+
+public class Prog45 {
+	public static void main(String args[])
+	{
+		int[] num= {12,56,78,9};
+		String[] str= {"dhivya","deva","tharanesswaren"};
+		char[] ch= {'a','b','c'};
+		for(int var:num)
+		{
+			System.out.println(var);
+		}
+		for(String result:str)
+		{
+			System.out.println(result);
+		}
+		for(char c:ch)
+		{
+			System.out.println(c);
+		}
+	}
+
+}
