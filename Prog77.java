113b7cc8d7a4fd59bd3ca383e792db8c48532fae
DHIVYADHARSHINIR637 <dhivyadharshinid589@gmail.com>
2025-06-08 19:33:09 +0530

    Create Prog77.java

---
+++ Prog77.java
@@ -0,0 +1,15 @@
+package dhivya1;
+interface A{
+	void display();
+	
+	
+}
+
+public class Prog77 {
+	public static void main(String args[])
+	{
+		A obj=()->{System.out.println("hello");};
+		obj.display();
+	}
+
+}
