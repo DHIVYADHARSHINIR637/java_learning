41ebaa6aa23a00a783b63338721c37057d471586
DHIVYADHARSHINIR637 <dhivyadharshinid589@gmail.com>
2025-06-09 19:41:34 +0530

    Create Prog83.java

---
+++ Prog83.java
@@ -0,0 +1,15 @@
+package dhivya;
+
+public class prog83 {
+	public static void main(String args[]) {
+	try {
+		int a=0;
+		throw new ArithmeticException("hello"); 
+	}
+	catch(Exception e) {
+		System.out.println(e);
+	}
+	}
+	
+
+}
