2f6507050d5e024c63f31aa3742f63b0baa1bf59
DHIVYADHARSHINIR637 <dhivyadharshinid589@gmail.com>
2025-09-13 21:44:01 +0530

    Create javaintToString.java

---
+++ javaintToString.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class javaintToString{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String s=Integer.toString(n);
+        if(n==Integer.parseInt(s))
+        {
+            System.out.println("Good Job");
+        }
+        else
+        {
+            System.out.println("Wrong answer");
+        }
+    }
+}
